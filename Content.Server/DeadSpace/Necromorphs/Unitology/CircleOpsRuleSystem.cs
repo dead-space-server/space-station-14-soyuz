f0fd80f96dbb41078a5b980f7481f46625f1aeb5
PERed <danielredov123@gmail.com>
2026-01-07 15:20:18 +0300

    fix3

---
+++ Content.Server/DeadSpace/Necromorphs/Unitology/CircleOpsRuleSystem.cs
@@ -0,0 +1,211 @@
+// Мёртвый Космос, Licensed under custom terms with restrictions on public hosting and commercial use, full text: https://raw.githubusercontent.com/dead-space-server/space-station-14-fobos/master/LICENSE.TXT
+
+using Content.Shared.GameTicking.Components;
+using Robust.Shared.Prototypes;
+using Content.Shared.Cargo.Prototypes;
+using Content.Server.GameTicking.Rules;
+using Content.Server.NukeOps;
+using Content.Server.Station.Components;
+using Content.Shared.NPC.Components;
+using Content.Shared.NPC.Systems;
+using Content.Shared.NPC.Prototypes;
+using Robust.Shared.Random;
+using Content.Shared.Shuttles.Components;
+using Content.Shared.DeadSpace.TimeWindow;
+using Content.Shared.NukeOps;
+using Content.Server.AlertLevel;
+using Content.Shared.Cargo.Components;
+using Content.Server.DeadSpace.ERT;
+using Content.Shared.DeadSpace.ERT.Prototypes;
+using Content.Server.Cargo.Systems;
+using Content.Server.GameTicking.Rules.Components;
+using Content.Server.RoundEnd;
+using Content.Shared.DeadSpace.Necromorphs.Necroobelisk;
+using Content.Server.DeadSpace.NoShuttleFTL;
+using Content.Server.GameTicking;
+
+namespace Content.Server.DeadSpace.Necromorphs.Unitology;
+
+public sealed class CircleOpsRuleSystem : GameRuleSystem<CircleOpsRuleComponent>
+{
+    [Dependency] private readonly NpcFactionSystem _npcFaction = default!;
+    [Dependency] private readonly IRobustRandom _random = default!;
+    [Dependency] private readonly TimedWindowSystem _timedWindow = default!;
+    [Dependency] private readonly AlertLevelSystem _alertLevel = default!;
+    [Dependency] private readonly ErtResponceSystem _ertResponceSystem = default!;
+    [Dependency] private readonly CargoSystem _cargoSystem = default!;
+    [Dependency] private readonly RoundEndSystem _roundEndSystem = default!;
+    private static readonly ProtoId<CargoAccountPrototype> Account = "Cargo";
+    private static readonly ProtoId<NpcFactionPrototype> Faction = "Necromorfs";
+    private static readonly ProtoId<ErtTeamPrototype> ErtTeam = "BSAA";
+    private static readonly TimeSpan CountdownRoundEndTime = TimeSpan.FromSeconds(30);
+    private static readonly EntProtoId ObeliskRule = "GiftNecroobeliskArtefact";
+    private static string AlertLevel { get; } = "sierra";
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<WarDeclaredEvent>(OnWarDeclared);
+        SubscribeLocalEvent<CircleOpsRuleComponent, RuleLoadedGridsEvent>(OnRuleLoadedGrids);
+        SubscribeLocalEvent<CircleOpsRuleComponent, StageObeliskEvent>(OnStageObelisk);
+        SubscribeLocalEvent<CircleOpsRuleComponent, SpawnNecroMoonEvent>(OnSpawnNecroMoon);
+    }
+
+    protected override void AppendRoundEndText(EntityUid uid, CircleOpsRuleComponent component, GameRuleComponent gameRule,
+        ref RoundEndTextAppendEvent args)
+    {
+        base.AppendRoundEndText(uid, component, gameRule, ref args);
+
+        if (component.State == CircleOpsState.ObeliskActivated)
+        {
+            args.AddLine(Loc.GetString("uni-ops-win"));
+        }
+        else
+        {
+            args.AddLine(Loc.GetString("uni-ops-loose"));
+        }
+    }
+
+
+    protected override void Started(EntityUid uid, CircleOpsRuleComponent component, GameRuleComponent gameRule, GameRuleStartedEvent args)
+    {
+        base.Started(uid, component, gameRule, args);
+
+        _timedWindow.Reset(component.WindowBefoteWarDeclaration);
+        _timedWindow.Reset(component.WindowUntilSendObelisk);
+
+        var eligible = new List<Entity<StationEventEligibleComponent, NpcFactionMemberComponent>>();
+        var eligibleQuery = EntityQueryEnumerator<StationEventEligibleComponent, NpcFactionMemberComponent>();
+        while (eligibleQuery.MoveNext(out var eligibleUid, out var eligibleComp, out var member))
+        {
+            if (!_npcFaction.IsFactionHostile(Faction, (eligibleUid, member)))
+                continue;
+
+            eligible.Add((eligibleUid, eligibleComp, member));
+        }
+
+        if (eligible.Count == 0)
+            return;
+
+        component.TargetStation = _random.Pick(eligible);
+    }
+
+    protected override void ActiveTick(EntityUid uid, CircleOpsRuleComponent component, GameRuleComponent gameRule, float frameTime)
+    {
+        base.ActiveTick(uid, component, gameRule, frameTime);
+
+        if (_timedWindow.IsExpired(component.WindowUntilSendObelisk)
+            && component.State == CircleOpsState.Inactive)
+        {
+            component.State = CircleOpsState.Preparing;
+            GameTicker.StartGameRule(ObeliskRule);
+        }
+
+        if (component.State == CircleOpsState.WarDeclared
+            && _timedWindow.IsExpired(component.WindowAfterWarDeclare)
+            && component.Shuttle.HasValue
+            && HasComp<NoShuttleFTLComponent>(component.Shuttle.Value))
+        {
+            RemComp<NoShuttleFTLComponent>(component.Shuttle.Value);
+        }
+
+        if (component.State == CircleOpsState.ObeliskActivated
+            && _timedWindow.IsExpired(component.WindowUntilSpawnMoon)
+            && component.Obelisk.HasValue
+            && EntityManager.EntityExists(component.Obelisk.Value)
+            && TryComp<NecroobeliskComponent>(component.Obelisk.Value, out var obeliskComp)
+            && !obeliskComp.IsStageConvergence)
+        {
+            obeliskComp.IsStageConvergence = true;
+        }
+    }
+
+    private void OnRuleLoadedGrids(Entity<CircleOpsRuleComponent> ent, ref RuleLoadedGridsEvent args)
+    {
+        var query = EntityQueryEnumerator<NoShuttleFTLComponent>();
+        while (query.MoveNext(out var uid, out _))
+        {
+            if (Transform(uid).MapID == args.Map)
+            {
+                ent.Comp.Shuttle = uid;
+                break;
+            }
+        }
+    }
+
+    private void OnStageObelisk(Entity<CircleOpsRuleComponent> ent, ref StageObeliskEvent args)
+    {
+        ent.Comp.Obelisk = args.Obelisk;
+        _timedWindow.Reset(ent.Comp.WindowUntilSpawnMoon);
+
+        ent.Comp.State = CircleOpsState.ObeliskActivated;
+    }
+
+    private void OnSpawnNecroMoon(Entity<CircleOpsRuleComponent> ent, ref SpawnNecroMoonEvent args)
+    {
+        _roundEndSystem.EndRound(CountdownRoundEndTime);
+    }
+
+    private void OnWarDeclared(ref WarDeclaredEvent ev)
+    {
+        var query = EntityQueryEnumerator<CircleOpsRuleComponent>();
+        while (query.MoveNext(out var uid, out var component))
+        {
+            if (_timedWindow.IsExpired(component.WindowBefoteWarDeclaration)
+                && component.State == CircleOpsState.Preparing)
+            {
+                component.State = CircleOpsState.WarReady;
+            }
+
+            if (TryComp<RuleGridsComponent>(uid, out var grids) && Transform(ev.DeclaratorEntity).MapID != grids.Map)
+                continue;
+
+            var newStatus = GetWarDeclaredStatus(component);
+            ev.Status = newStatus;
+
+            if (component.TargetStation == null
+                || newStatus != WarConditionStatus.WarReady)
+                continue;
+
+            component.State = CircleOpsState.WarDeclared;
+            _timedWindow.Reset(component.WindowAfterWarDeclare);
+
+            _alertLevel.SetLevel(component.TargetStation.Value, AlertLevel, false, true, true);
+
+            if (!TryComp<StationBankAccountComponent>(component.TargetStation, out var stationAccount))
+                return;
+
+            var addMoneyAfterWarDeclared = _ertResponceSystem.GetErtPrice(ErtTeam);
+
+            _cargoSystem.UpdateBankAccount(
+                                (component.TargetStation.Value, stationAccount),
+                                addMoneyAfterWarDeclared,
+                                Account
+                            );
+        }
+    }
+
+    private WarConditionStatus? GetWarDeclaredStatus(CircleOpsRuleComponent component)
+    {
+        if (component.State == CircleOpsState.Inactive)
+            return null;
+
+        if (!_timedWindow.IsExpired(component.WindowBefoteWarDeclaration))
+            return null;
+
+        switch (component.State)
+        {
+            case CircleOpsState.Preparing:
+                return WarConditionStatus.NoWarSmallCrew;
+            case CircleOpsState.WarReady:
+                return WarConditionStatus.WarReady;
+            case CircleOpsState.WarDeclared:
+                return WarConditionStatus.YesWar;
+            case CircleOpsState.ObeliskActivated:
+                return WarConditionStatus.YesWar;
+            default:
+                return WarConditionStatus.NoWarSmallCrew;
+        }
+    }
+}
---
+++ Content.Server/DeadSpace/Necromorphs/Unitology/Components/CircleOpsRuleComponent.cs
@@ -0,0 +1,59 @@
+// Мёртвый Космос, Licensed under custom terms with restrictions on public hosting and commercial use, full text: https://raw.githubusercontent.com/dead-space-server/space-station-14-fobos/master/LICENSE.TXT
+
+using Content.Shared.DeadSpace.TimeWindow;
+
+namespace Content.Server.DeadSpace.Necromorphs.Unitology;
+
+
+[RegisterComponent, Access(typeof(CircleOpsRuleSystem))]
+public sealed partial class CircleOpsRuleComponent : Component
+{
+    /// <summary>
+    ///    Окно времени до появления возможности обьявить войну
+    /// </summary>
+    [DataField]
+    [ViewVariables(VVAccess.ReadOnly)]
+    public TimedWindow WindowBefoteWarDeclaration = new(TimeSpan.FromMinutes(20), TimeSpan.FromMinutes(20));
+
+    /// <summary>
+    ///    Окно времени до появления возможности прилететь на станцию после обьявления войны
+    /// </summary>
+    [DataField]
+    [ViewVariables(VVAccess.ReadOnly)]
+    public TimedWindow WindowAfterWarDeclare = new(TimeSpan.FromMinutes(15), TimeSpan.FromMinutes(15));
+
+    /// <summary>
+    ///    Окно времени до спавна обелиска на станцию
+    /// </summary>
+    [DataField]
+    [ViewVariables(VVAccess.ReadOnly)]
+    public TimedWindow WindowUntilSendObelisk = new(TimeSpan.FromMinutes(1), TimeSpan.FromMinutes(1));
+
+    /// <summary>
+    ///    Окно времени до появления луны
+    /// </summary>
+    [DataField]
+    [ViewVariables(VVAccess.ReadOnly)]
+    public TimedWindow WindowUntilSpawnMoon = new(TimeSpan.FromMinutes(15), TimeSpan.FromMinutes(15));
+
+    [ViewVariables(VVAccess.ReadOnly)]
+    public EntityUid? TargetStation;
+
+    [ViewVariables(VVAccess.ReadOnly)]
+    public EntityUid? Shuttle;
+
+    [ViewVariables(VVAccess.ReadOnly)]
+    public EntityUid? Obelisk;
+
+    [ViewVariables(VVAccess.ReadOnly)]
+    public CircleOpsState State = CircleOpsState.Inactive;
+}
+
+public enum CircleOpsState : byte
+{
+    Inactive,
+    Preparing,
+    WarReady,
+    WarDeclared,
+    ObeliskActivated
+}

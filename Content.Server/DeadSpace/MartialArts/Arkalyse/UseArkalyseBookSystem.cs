5dd4b17e3bb8ca112b5265d44b141236ee258d86
TheZuTi <152662867+TheZuTi@users.noreply.github.com>
2026-05-05 16:14:06 +0300

    Аркалисы и Курящий карп от Мали (#1277)

---
+++ Content.Server/DeadSpace/MartialArts/Arkalyse/UseArkalyseBookSystem.cs
@@ -0,0 +1,52 @@
+// Мёртвый Космос, Licensed under custom terms with restrictions on public hosting and commercial use, full text: https://raw.githubusercontent.com/dead-space-server/space-station-14-fobos/master/LICENSE.TXT
+using Content.Server.DeadSpace.MartialArts.Components;
+using Content.Shared.Interaction.Events;
+using Content.Shared.Weapons.Melee;
+using Content.Shared.Actions;
+using Content.Shared.Popups;
+using Content.Server.DeadSpace.MartialArts.SmokingCarp.Components;
+using Content.Server.DeadSpace.MartialArts.Arkalyse.Components;
+using Robust.Server.GameObjects;
+
+namespace Content.Server.DeadSpace.MartialArts;
+
+public sealed class UseArkalyseBookSystem : EntitySystem
+{
+    [Dependency] private readonly SharedActionsSystem _action = default!;
+    [Dependency] private readonly TransformSystem _transform = default!;
+    [Dependency] private readonly SharedPopupSystem _popup = default!;
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeLocalEvent<MartialArtsTrainingArkalyseComponent, UseInHandEvent>(OnUseBookArkalyse);
+    }
+    private void OnUseBookArkalyse(Entity<MartialArtsTrainingArkalyseComponent> ent, ref UseInHandEvent args)
+    {
+        if (args.Handled || TryComp<SmokingCarpComponent>(args.User, out _))
+            return;
+
+        if (TryComp<ArkalyseComponent>(args.User, out var existing))
+        {
+            if (!existing.LearnedFromManual)
+                _popup.PopupEntity(Loc.GetString("arkalyse-remove-gloves-before-learn"), args.User, args.User);
+
+            args.Handled = true;
+            return;
+        }
+
+        var userArkalyse = EnsureComp<ArkalyseComponent>(args.User);
+        userArkalyse.LearnedFromManual = true;
+        userArkalyse.Params = ent.Comp.Params[0];
+
+        foreach (var actionId in userArkalyse.BaseArkalyse)
+            _action.AddAction(args.User, actionId);
+
+        if (TryComp<MeleeWeaponComponent>(args.User, out var melee))
+            melee.AttackRate = ent.Comp.AddAtackRate;
+
+        Del(ent);
+        Spawn(ent.Comp.ItemAfterLerning, _transform.GetMapCoordinates(args.User));
+
+        args.Handled = true;
+    }
+}
---
+++ Content.Server/DeadSpace/MartialArts/SmokingCarp/UseInjectorSmokingCarpSystem.cs
@@ -0,0 +1,46 @@
+// Мёртвый Космос, Licensed under custom terms with restrictions on public hosting and commercial use, full text: https://raw.githubusercontent.com/dead-space-server/space-station-14-fobos/master/LICENSE.TXT
+using Content.Server.DeadSpace.MartialArts.Components;
+using Content.Shared.Interaction.Events;
+using Content.Shared.Weapons.Melee;
+using Content.Shared.Actions;
+using Content.Server.DeadSpace.MartialArts.Arkalyse.Components;
+using Content.Server.DeadSpace.MartialArts.SmokingCarp.Components;
+using Robust.Server.GameObjects;
+using Content.Shared.DeadSpace.MartialArts.SmokingCarp.Components;
+
+namespace Content.Server.DeadSpace.MartialArts.SmokingCarp;
+public sealed class UseArkalyseBookSystem : EntitySystem
+{
+    [Dependency] private readonly SharedActionsSystem _action = default!;
+    [Dependency] private readonly TransformSystem _transform = default!;
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeLocalEvent<MartialArtsTrainingCarpComponent, UseInHandEvent>(OnUseInjectorSmokingCarp);
+    }
+
+    private void OnUseInjectorSmokingCarp(Entity<MartialArtsTrainingCarpComponent> ent, ref UseInHandEvent args)
+    {
+        if (args.Handled || TryComp<ArkalyseComponent>(args.User, out _))
+            return;
+
+        if (HasComp<SmokingCarpComponent>(args.User))
+            return;
+
+        EnsureComp<SmokingCarpTripPunchComponent>(args.User);
+        EnsureComp<SmokingCarpNotShotComponent>(args.User);
+        var userSmokingCarp = EnsureComp<SmokingCarpComponent>(args.User);
+        userSmokingCarp.Params = ent.Comp.Params[0];
+
+        foreach (var actionId in userSmokingCarp.BaseSmokingCarp)
+            _action.AddAction(args.User, actionId);
+
+        if (TryComp<MeleeWeaponComponent>(args.User, out var melee))
+            melee.AttackRate = ent.Comp.AddAtackRate;
+
+        Del(ent);
+        Spawn(ent.Comp.ItemAfterLerning, _transform.GetMapCoordinates(args.User));
+
+        args.Handled = true;
+    }
+}

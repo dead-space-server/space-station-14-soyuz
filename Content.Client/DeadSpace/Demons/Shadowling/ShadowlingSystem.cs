be8b37a46a5045e0756e90534c2a3026ceab070c
PERed <danielredov123@gmail.com>
2026-05-11 21:34:08 +0300

    иэто

--- Content.Client/DeadSpace/Demons/Shadowling/ShadowlingSystem.cs
+++ Content.Client/DeadSpace/Demons/Shadowling/ShadowlingSystem.cs
@@ -1,7 +1,7 @@
 // Мёртвый Космос, Licensed under custom terms with restrictions on public hosting and commercial use, full text: https://raw.githubusercontent.com/dead-space-server/space-station-14-fobos/master/LICENSE.TXT
 
-using Content.Shared.DeadSpace.EntityFilterBarrier;
+using Content.Shared.DeadSpace.Demons.Shadowling;
 
-namespace Content.Client.DeadSpace.EntityFilterBarrier;
+namespace Content.Client.DeadSpace.Demons.Shadowling;
 
-public sealed class EntityFilterBarrierSystem : SharedEntityFilterBarrierSystem { }
+public sealed class ShadowlingSystem : SharedShadowlingSystem;

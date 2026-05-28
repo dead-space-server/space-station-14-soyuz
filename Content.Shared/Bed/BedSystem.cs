36a1a0fc88777d9a2537cff27847404bbc2959c1
PERed <danielredov123@gmail.com>
2026-05-28 10:28:17 +0300

    ger

--- Content.Shared/Bed/BedSystem.cs
+++ Content.Shared/Bed/BedSystem.cs
@@ -4,28 +4,21 @@ using Content.Shared.Body.Systems; // DS14
 using Content.Shared.Bed.Components;
 using Content.Shared.Bed.Sleep;
 using Content.Shared.Buckle.Components;
-using Content.Shared.Damage.Systems;
 using Content.Shared.Emag.Systems;
-using Content.Shared.Mobs.Systems;
 using Content.Shared.Power;
 using Content.Shared.Power.EntitySystems;
 using Robust.Shared.Timing;
 
 namespace Content.Shared.Bed;
 
-public sealed class BedSystem : EntitySystem
+public abstract class SharedBedSystem : EntitySystem
 {
     [Dependency] protected readonly IGameTiming Timing = default!;
     [Dependency] private readonly SharedActionsSystem _actionsSystem = default!;
     [Dependency] private readonly EmagSystem _emag = default!;
-    [Dependency] private readonly IGameTiming _timing = default!;
-    [Dependency] private readonly MobStateSystem _mobStateSystem = default!;
-    [Dependency] private readonly SharedActionsSystem _actionsSystem = default!;
     [Dependency] private readonly SharedMetabolizerSystem _metabolizer = default!;
     [Dependency] private readonly SharedPowerReceiverSystem _powerReceiver = default!;
     [Dependency] private readonly SleepingSystem _sleepingSystem = default!;
-
-    private EntityQuery<SleepingComponent> _sleepingQuery;
 
     public override void Initialize()
     {
@@ -40,8 +33,6 @@ public sealed class BedSystem : EntitySystem
         SubscribeLocalEvent<StasisBedComponent, GotEmaggedEvent>(OnStasisEmagged);
         SubscribeLocalEvent<StasisBedComponent, PowerChangedEvent>(OnPowerChanged);
         SubscribeLocalEvent<StasisBedBuckledComponent, GetMetabolicMultiplierEvent>(OnStasisGetMetabolicMultiplier);
-
-        _sleepingQuery = GetEntityQuery<SleepingComponent>();
     }
 
     private void OnHealMapInit(Entity<HealOnBuckleComponent> ent, ref MapInitEvent args)
@@ -123,7 +114,7 @@ public sealed class BedSystem : EntitySystem
         args.Multiplier *= stasis.Multiplier;
     }
 
-    private void UpdateMetabolisms(Entity<StrapComponent?> ent)
+    protected void UpdateMetabolisms(Entity<StrapComponent?> ent)
     {
         if (!Resolve(ent, ref ent.Comp, false))
             return;
@@ -131,38 +122,6 @@ public sealed class BedSystem : EntitySystem
         foreach (var buckledEntity in ent.Comp.BuckledEntities)
         {
             _metabolizer.UpdateMetabolicMultiplier(buckledEntity);
-        }
-    }
-
-    public override void Update(float frameTime)
-    {
-        base.Update(frameTime);
-
-        var query = EntityQueryEnumerator<HealOnBuckleHealingComponent, HealOnBuckleComponent, StrapComponent>();
-        while (query.MoveNext(out var uid, out _, out var bedComponent, out var strapComponent))
-        {
-            if (_timing.CurTime < bedComponent.NextHealTime)
-                continue;
-
-            bedComponent.NextHealTime += TimeSpan.FromSeconds(bedComponent.HealTime);
-
-            Dirty(uid, bedComponent);
-
-            if (strapComponent.BuckledEntities.Count == 0)
-                continue;
-
-            foreach (var healedEntity in strapComponent.BuckledEntities)
-            {
-                if (_mobStateSystem.IsDead(healedEntity))
-                    continue;
-
-                var damage = bedComponent.Damage;
-
-                if (_sleepingQuery.HasComp(healedEntity))
-                    damage *= bedComponent.SleepMultiplier;
-
-                _damageableSystem.TryChangeDamage(healedEntity, damage, true, origin: uid);
-            }
         }
     }
 }

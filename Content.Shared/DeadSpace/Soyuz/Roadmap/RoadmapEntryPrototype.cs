b5c692c8806d7f0efe2084f6bafbe520e9cf03b7
PERed <danielredov123@gmail.com>
2026-06-06 19:17:14 +0300

    Ладно похуй пока без локализации

--- Content.Shared/DeadSpace/Soyuz/Roadmap/RoadmapEntryPrototype.cs
+++ Content.Shared/DeadSpace/Soyuz/Roadmap/RoadmapEntryPrototype.cs
@@ -13,60 +13,33 @@ public sealed partial class RoadmapEntryPrototype : IPrototype
     public string ID { get; private set; } = default!;
     
     /// <summary>
-    /// Ключ локализации для названия карточки.
-    /// Если указан — используется вместо Title.
-    /// Пример: "roadmap-entry-feature-title"
+    /// Название карточка
     /// </summary>
-    [DataField("locTitle")]
-    public string? LocTitle { get; private set; }
-
-    /// <summary>
-    /// Название карточки.
-    /// Используется, если LocTitle не задан.
-    /// </summary>
-    [DataField("title")]
+    [DataField("title", required: true)]
     public string Title { get; private set; } = string.Empty;
     
     /// <summary>
-    /// Ключ локализации для описания карточки.
-    /// Если указан — используется вместо Description.
-    /// Пример: "roadmap-entry-feature-desc"
+    /// Описание карточки. Короткое описание отображается до 30 символов, далее троеточие. Полное описание требуется раскрыть
     /// </summary>
-    [DataField("locDescription")]
-    public string? LocDescription { get; private set; }
-
-    /// <summary>
-    /// Описание карточки. Короткое описание отображается до 30 символов, далее троеточие. Полное описание требуется раскрыть.
-    /// Используется, если LocDescription не задан.
-    /// </summary>
-    [DataField("description")]
+    [DataField("description", required: true)]
     public string Description { get; private set; } = string.Empty;
     
     /// <summary>
-    /// Категория карточки, в которой карточка будет отображаться.
+    /// Категория карточки, в которой карточка будет отображаться
     /// Completed, InProgress, Planned
     /// </summary>
     [DataField("category", required: true)]
     public RoadmapCategory Category { get; private set; } = RoadmapCategory.Planned;
     
     /// <summary>
-    /// Порядок отображения карточек в колонке.
-    /// Записи с меньшим значением отображаются выше.
+    /// Порядок отображения карточек в колонке
+    /// Записи с меньшим значением отображаются выше
     /// </summary>
     [DataField("order")]
     public int Order { get; private set; } = 0;
     
     /// <summary>
-    /// Ключи локализации для тегов.
-    /// Если указаны — используются вместо Tags.
-    /// Пример: ["roadmap-tag-gameplay", "roadmap-tag-major"]
-    /// </summary>
-    [DataField("locTags")]
-    public List<string>? LocTags { get; private set; }
-
-    /// <summary>
     /// Теги, отображаемые снизу карточки. Сортировки по тегам нет :(
-    /// Используются, если LocTags не задан.
     /// </summary>
     [DataField("tags")]
     public List<string> Tags { get; private set; } = new();

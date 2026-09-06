werj546/space-station-14-fobos

ГАЗЫ ЧАСТЬ ДВА! НИО РЕВОРК ЧАСТЬ 0.6 (#2261)
Co-authored-by: Алексей Герасимов <gerasimovav@althub.pro>

Resources/Locale/en-US/spray-painter/spray-painter.ftl
@@ -194,14 +194,18 @@ spray-painter-style-canisters-plasma = Plasma
 spray-painter-style-canisters-storage = Storage
 spray-painter-style-canisters-tritium = Tritium
 spray-painter-style-canisters-water-vapor = Water vapor
-spray-painter-style-canisters-nitryl = Nitrogen dioxide
 spray-painter-style-canisters-fixirium = Fixirium
 spray-painter-style-canisters-hydrogen = Hydrogen
 spray-painter-style-canisters-nitriatium = Nitriatium
 spray-painter-style-canisters-hilium = Hilium
 spray-painter-style-canisters-brizidium = Brizidium
 spray-painter-style-canisters-iprit = Iprit
-spray-painter-style-canisters-nitric-oxide = Nitric oxide
 spray-painter-style-canisters-oxyhydrogen = Oxyhydrogen
 spray-painter-style-canisters-fuel = Fuel mix
 spray-painter-style-canisters-necrogas = Necrogas
+spray-painter-style-canisters-hyper-noblium = Hyper-Noblium
+spray-painter-style-canisters-proto-nitrate = Proto-Nitrate
+spray-painter-style-canisters-helium = Helium
+spray-painter-style-canisters-halon = Halon
+spray-painter-style-canisters-zauker = Zauker
+spray-painter-style-canisters-antinoblium = Anti-Noblium

dead-space-server/space-station-14-soyuz

Resources/Locale/en-US/spray-painter/spray-painter.ftl
@@ -186,18 +186,12 @@ spray-painter-style-cratesecure-weapon = Weapon
 spray-painter-style-canisters-air = Air
 spray-painter-style-canisters-ammonia = Ammonia
 # Kofeecheks canister painter styles: LicenseRef-Kofeecheks
-spray-painter-style-canisters-brizidium = Brizidium
 spray-painter-style-canisters-carbon-dioxide = Carbon dioxide
 spray-painter-style-canisters-frezon = Frezon
-spray-painter-style-canisters-fixirium = Fixirium
-spray-painter-style-canisters-hilium = Hilium
-spray-painter-style-canisters-hydrogen = Hydrogen
-spray-painter-style-canisters-iprit = Iprit
 spray-painter-style-canisters-liquid-carbon-dioxide = Liquid carbon dioxide
 spray-painter-style-canisters-liquid-nitrogen = Liquid nitrogen
 spray-painter-style-canisters-liquid-oxygen = Liquid oxygen
 spray-painter-style-canisters-nitrogen = Nitrogen
-spray-painter-style-canisters-nitriatium = Nitriatium
 spray-painter-style-canisters-nitryl = Nitrogen dioxide
 spray-painter-style-canisters-nitrous-oxide = Nitrous oxide
 spray-painter-style-canisters-oxygen = Oxygen
@@ -205,6 +199,12 @@ spray-painter-style-canisters-plasma = Plasma
 spray-painter-style-canisters-storage = Storage
 spray-painter-style-canisters-tritium = Tritium
 spray-painter-style-canisters-water-vapor = Water vapor
+spray-painter-style-canisters-fixirium = Fixirium
+spray-painter-style-canisters-hydrogen = Hydrogen
+spray-painter-style-canisters-nitriatium = Nitriatium
+spray-painter-style-canisters-hilium = Hilium
+spray-painter-style-canisters-brizidium = Brizidium
+spray-painter-style-canisters-iprit = Iprit
 spray-painter-style-canisters-oxyhydrogen = Oxyhydrogen
 spray-painter-style-canisters-fuel = Fuel mix
 spray-painter-style-canisters-necrogas = Necrogas
7111fe545a056d4216b7411314e702720a42f1fb
PERed <danielredov123@gmail.com>
2026-09-14 02:08:10 +0300

    рпврврвервеараве

--- Content.Server/DeadSpace/Atmos/Hallucinations/ParacusiaStatusEffectComponent.cs
+++ Content.Server/DeadSpace/Atmos/Hallucinations/ParacusiaStatusEffectComponent.cs
@@ -1,7 +1,6 @@
 // Мёртвый Космос, Licensed under custom terms with restrictions on public hosting and commercial use, full text: https://raw.githubusercontent.com/dead-space-server/space-station-14-fobos/master/LICENSE.TXT
 
-namespace Content.Server.DeadSpace.Arena;
+namespace Content.Server.DeadSpace.Atmos.Hallucinations;
 
 [RegisterComponent]
-public sealed partial class ArenaSpawnPointComponent : Component
-{ }
+public sealed partial class ParacusiaStatusEffectComponent : Component;
